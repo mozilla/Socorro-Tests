c6f0bb4f55bec63c94d7690a4540bdfd49848d99
dburns@mozilla.com <dburns@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2010-11-23 22:03:43 +0000

    Updated vars to integrate with grid
    git-svn-id: https://svn.mozilla.org/projects/socorro_qa@78012 4eb1ac78-321c-0410-a911-ec516a8615a5

--- vars.py
+++ vars.py
@@ -1,7 +1,7 @@
 class ConnectionParameters:
-    server = 'localhost' #"qa-selenium.mv.mozilla.com"
+    server = "qa-selenium.mv.mozilla.com"
     port = 4444
-    browser = "*firefox" #"Firefox-default-b;en-us;MacOSX6"
+        browser = "Firefox-default-b;en-us;MacOSX6"
     #browser = "*iexplore"
     baseurl = "http://crash-stats.stage.mozilla.com"
     page_load_timeout = 120000
