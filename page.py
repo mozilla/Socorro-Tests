edf55fe8346c7d9a99f147bc4398775d305fa678
dburns@mozilla.com <dburns@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-02-08 19:20:52 +0000

    Added new helper method
    git-svn-id: https://svn.mozilla.org/projects/socorro_qa@82293 4eb1ac78-321c-0410-a911-ec516a8615a5

--- page.py
+++ page.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+#
 # ***** BEGIN LICENSE BLOCK *****
 # Version: MPL 1.1/GPL 2.0/LGPL 2.1
 #
@@ -11,14 +13,16 @@
 # for the specific language governing rights and limitations under the
 # License.
 #
-# The Original Code is Crash Tests Selenium Tests.
+# The Original Code is Firefox Input 
 #
 # The Initial Developer of the Original Code is
-# Mozilla.
+# Mozilla Corp.
 # Portions created by the Initial Developer are Copyright (C) 2010
 # the Initial Developer. All Rights Reserved.
 #
-# Contributor(s): David Burns
+# Contributor(s): Vishal
+#                 Dave Hunt
+#                 David Burns
 #
 # Alternatively, the contents of this file may be used under the terms of
 # either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -33,8 +37,18 @@
 # the terms of any one of the MPL, the GPL or the LGPL.
 #
 # ***** END LICENSE BLOCK *****
-import re
+'''
+Created on Jun 21, 2010
 
+'''
+import re
+import time
+import vars
+import base64
+
+page_load_timeout = vars.ConnectionParameters.page_load_timeout
+base_url = vars.ConnectionParameters.baseurl
+http_regex = re.compile('https?://((\w+\.)+\w+\.\w+)')
 
 
 class Page(object):
@@ -42,19 +56,24 @@ class Page(object):
     Base class for all Pages
     '''
 
-        
     def __init__(self, selenium):
         '''
         Constructor
         '''
         self.selenium = selenium
-        
-        
-    def verify_page_title(self, title_regexp):
-        if (re.search(title_regexp, self.selenium.get_title()) is None):
-            raise Exception,'\r\nPage tile verification failed. Expected: %s; Actual:%s\r\n' %(title_regexp,self.selenium.get_title())
-       
-        
+
+    @property
+    def is_the_current_page(self):
+        page_title = self.selenium.get_title()
+        if not page_title == self._page_title:
+            self.record_error()
+            try:
+                raise Exception("Expected page title to be: '" + self._page_title + "' but it was: '" + page_title + "'")
+            except Exception:
+                raise Exception('Expected page title does not match actual page title.')
+        else:
+            return True
+
     def click_link(self, link, wait_flag=False,timeout=80000):
         self.selenium.click("link=%s" %(link))
         if(wait_flag):
@@ -78,6 +97,9 @@ class Page(object):
     
     def is_element_present(self,locator):
         return self.selenium.is_element_present(locator)
+
+    def is_element_visible(self, locator):
+        return self.selenium.is_visible(locator)
     
     def is_text_present(self,text):
         return self.selenium.is_text_present(text)
@@ -85,3 +107,58 @@ class Page(object):
     def refresh(self,timeout=80000):
         self.selenium.refresh()
         self.selenium.wait_for_page_to_load(timeout)
+
+    def wait_for_element_present(self, element):
+        count = 0
+        while not self.is_element_present(element):
+            time.sleep(1)
+            count += 1
+            if count == page_load_timeout/1000:
+                self.record_error()
+                raise Exception(element + ' has not loaded')
+
+    def wait_for_element_visible(self, element):
+        self.wait_for_element_present(element)
+        count = 0
+        while not self.is_element_visible(element):
+            time.sleep(1)
+            count += 1
+            if count == page_load_timeout/1000:
+                self.record_error()
+                raise Exception(element + " is not visible")
+
+    def wait_for_element_not_visible(self, element):
+        count = 0
+        while self.is_element_visible(element):
+            time.sleep(1)
+            count += 1
+            if count == page_load_timeout/1000:
+                self.record_error()
+                raise Exception(element + " is still visible")
+
+    def wait_for_page(self, url_regex):
+        count = 0
+        while (re.search(url_regex, self.selenium.get_location(), re.IGNORECASE)) is None:
+            time.sleep(1)
+            count += 1
+            if count == page_load_timeout/1000:
+                self.record_error()
+                raise Exception("Sites Page has not loaded")
+
+    def record_error(self):
+        ''' Records an error. '''
+
+        http_matches = http_regex.match(base_url)
+        file_name = http_matches.group(1)
+
+        print '-------------------'
+        print 'Error at ' + self.selenium.get_location()
+        print 'Page title ' + self.selenium.get_title()
+        print '-------------------'
+        filename = file_name + '_' + str(time.time()).split('.')[0] + '.png'
+
+        print 'Screenshot of error in file ' + filename
+        f = open(filename, 'wb')
+        f.write(base64.decodestring(
+            self.selenium.capture_entire_page_screenshot_to_string('')))
+        f.close()
