536e29297876d4671d293968727f4a4cd884cb72
Tobias Markus <tobbi.bugs@mozilla.com>
2011-08-10 13:07:42 -0700

    Adding type parameter to parser.addoption

--- conftest.py
+++ conftest.py
@@ -71,7 +71,7 @@ def pytest_addoption(parser):
         help="specify the browser")
     parser.addoption("--site", action="store", default="https://crash-stats.allizom.org",
         help="specify the AUT")
-    parser.addoption("--timeout", action="store", default=120000,
+    parser.addoption("--timeout", action="store", type="int", default=120000,
         help="specify the timeout")
 
 class TestSetup:
