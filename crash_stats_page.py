e872e50911d090b237a73b3f795bb3f632741542
Alin <trifandreialin@gmail.com>
2011-10-26 17:36:31 +0300

    Removed get...

--- crash_stats_page.py
+++ crash_stats_page.py
@@ -423,7 +423,7 @@ class CrashStatsAdvancedSearch(CrashStatsBasePage):
         self.sel.type(self._build_id_locator, value)
 
     @property
-    def get_build_id(self):
+    def build_id(self):
         return self.sel.get_eval("navigator.buildID")
 
     @property
