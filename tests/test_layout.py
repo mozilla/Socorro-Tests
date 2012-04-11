bfea47aca20cb86ecc70e925dcb98419c09bc359
Zac <zac@zac-Ubuntu.(none)>
2012-04-11 18:54:37 +0100

    add nondestructive tags

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -12,6 +12,7 @@ xfail = pytest.mark.xfail
 
 class TestLayout:
 
+    @pytest.mark.nondestructive
     def test_that_products_are_sorted_correctly(self, mozwebqa):
 
         csp = CrashStatsHomePage(mozwebqa)
@@ -20,13 +21,15 @@ class TestLayout:
         products = [product.text for product in  csp.header.product_list]
         Assert.equal(product_list, products)
 
-    @xfail(reason='Bug 687841 - Versions in Navigation Bar appear in wrong order')
+    @pytest.mark.xfail(reason='Bug 687841 - Versions in Navigation Bar appear in wrong order')
+    @pytest.mark.nondestructive
     def test_that_product_versions_are_ordered_correctly(self, mozwebqa):
         csp = CrashStatsHomePage(mozwebqa)
 
         Assert.is_sorted_descending(csp.header.current_versions)
         Assert.is_sorted_descending(csp.header.other_versions)
 
+    @pytest.mark.nondestructive
     def test_that_topcrasher_is_not_returning_http500(self, mozwebqa):
         csp = CrashStatsHomePage(mozwebqa)
         csp.get_url_path(csp.base_url + '/topcrasher')
@@ -34,11 +37,13 @@ class TestLayout:
         Assert.contains('Top Crashers', tc.page_heading)
         Assert.true(tc.results_found, 'No results found!')
 
+    @pytest.mark.nondestructive
     def test_that_report_is_not_returning_http500(self, mozwebqa):
         csp = CrashStatsHomePage(mozwebqa)
         csp.get_url_path(csp.base_url + '/report')
         Assert.contains('Page not Found', csp.page_heading)
 
+    @pytest.mark.nondestructive
     def test_that_correlation_is_not_returning_http500(self, mozwebqa):
         csp = CrashStatsHomePage(mozwebqa)
         csp.get_url_path(csp.base_url + '/correlation')
