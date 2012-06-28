b05ac090334cc1966f15c1cd973a605f6891ea05
Matt Brandt <mbrandt@mozilla.com>
2012-06-28 12:33:00 -0700

    updated adv. search tests to filter on a reduced date range of results

--- tests/test_search.py
+++ tests/test_search.py
@@ -40,6 +40,8 @@ class TestSearchForIdOrSignature:
     def test_that_advanced_search_for_firefox_can_be_filtered(self, mozwebqa):
         csp = CrashStatsHomePage(mozwebqa)
         cs_advanced = csp.header.click_advanced_search()
+        cs_advanced.set_period_value_field_input('\b3')
+        cs_advanced.select_period_units('Days')
         cs_advanced.click_filter_reports()
         Assert.contains('product is one of Firefox', cs_advanced.results_lead_in_text)
 
@@ -84,6 +86,9 @@ class TestSearchForIdOrSignature:
 
         cs_advanced.adv_select_product('Firefox')
         cs_advanced.adv_select_version('All')
+        cs_advanced.set_period_value_field_input('\b3')
+        cs_advanced.select_period_units('Days')
+        
         cs_advanced.click_filter_reports()
 
         results_page_count = cs_advanced.results[0].number_of_crashes
