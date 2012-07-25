0206b4b0486155eb93f5597d34102051db90309f
Matt Brandt <mbrandt@mozilla.com>
2012-07-25 09:22:56 -0700

    fix dave's insightful pep8 nit

--- tests/test_search.py
+++ tests/test_search.py
@@ -15,7 +15,14 @@ prod = pytest.mark.prod
 
 class TestSearchForIdOrSignature:
 
-    _expected_products = ['Firefox', 'Thunderbird', 'SeaMonkey', 'Camino', 'Fennec', 'FennecAndroid', 'WebappRuntime']
+    _expected_products = [
+        'Firefox',
+        'Thunderbird',
+        'SeaMonkey',
+        'Camino',
+        'Fennec',
+        'FennecAndroid', 
+        'WebappRuntime']
 
     @pytest.mark.nondestructive
     def test_that_when_item_not_available(self, mozwebqa):
