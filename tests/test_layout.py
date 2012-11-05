62a7d93110d956064c5743e3c0513ea744c0669e
Stephen Donner <stephen.donner@gmail.com>
2012-11-05 12:48:26 -0800

    xfail for dev, too -- this is really only fixed in django

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -16,6 +16,7 @@ class TestLayout:
 
     @pytest.mark.nondestructive
     # xfail - bug 802479 - adjust sort order to use what is set in the db 
+    @pytest.mark.xfail("'crash-stats-dev.allizom.org' in config.getvalue('base_url')")
     @pytest.mark.xfail("'crash-stats.allizom.org' in config.getvalue('base_url')")
     @pytest.mark.xfail("'crash-stats.mozilla.com' in config.getvalue('base_url')")
     def test_that_products_are_sorted_correctly(self, mozwebqa):
