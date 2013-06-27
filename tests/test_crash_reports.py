0d96ffc76e6048672188a2372f0050ad69c5066f
Matt Brandt <mbrandt@mozilla.com>
2013-06-27 16:03:24 -0700

    update xfails to point at the php app

--- tests/test_crash_reports.py
+++ tests/test_crash_reports.py
@@ -113,7 +113,7 @@ class TestCrashReports:
 
     @pytest.mark.xfail("'crash-stats-dev.allizom.org' in config.getvalue('base_url')", reason="Bug 603561 - Top Changers option isn't highlighted when chosen")
     @pytest.mark.xfail("'crash-stats.allizom.org' in config.getvalue('base_url')", reason="Bug 603561 - Top Changers option isn't highlighted when chosen")
-    @pytest.mark.xfail("'crash-stats.mozilla.com' in config.getvalue('base_url')", reason="Bug 603561 - Top Changers option isn't highlighted when chosen")
+    @pytest.mark.xfail("'crash-stats-php.mozilla.org' in config.getvalue('base_url')", reason="Bug 603561 - Top Changers option isn't highlighted when chosen")
     @pytest.mark.nondestructive
     def test_that_top_changers_is_highlighted_when_chosen(self, mozwebqa):
         """
--- tests/test_layout.py
+++ tests/test_layout.py
@@ -18,7 +18,7 @@ class TestLayout:
     # xfail - bug 802479 - adjust sort order to use what is set in the db
     @pytest.mark.xfail("'crash-stats-dev.allizom.org' in config.getvalue('base_url')")
     @pytest.mark.xfail("'crash-stats.allizom.org' in config.getvalue('base_url')")
-    @pytest.mark.xfail("'crash-stats.mozilla.com' in config.getvalue('base_url')")
+    @pytest.mark.xfail("'crash-stats-php.mozilla.org' in config.getvalue('base_url')")
     def test_that_products_are_sorted_correctly(self, mozwebqa):
 
         csp = CrashStatsHomePage(mozwebqa)
