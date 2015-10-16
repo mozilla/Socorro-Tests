e1cbb3bdae090192aeaba8f47c8e3342adc33093
Dave Hunt <dave.hunt@gmail.com>
2015-10-16 17:34:59 +0100

    Revert "Mark top crasher for plugins tests as expected to fail due to bug 1213247"
    This reverts commit c50b3897d1c579ec7125af8457f9756e014eb26b.

--- tests/test_crash_reports.py
+++ tests/test_crash_reports.py
@@ -71,7 +71,6 @@ class TestCrashReports:
         assert cstc.results_count > 0
 
     @pytest.mark.nondestructive
-    @pytest.mark.xfail(reason='bug 1213247')
     def test_that_top_crasher_filter_plugin_return_results(self, mozwebqa):
         csp = CrashStatsHomePage(mozwebqa)
         product = csp.header.current_product
